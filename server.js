cd6f13d91ffc74130c33686408839bc293d94d74
Anukool Srivastava <anukoolsriv@gmail.com>
2017-02-08 23:56:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/song-one',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'song-one.html'));
 });
 
-app.get('/article-two',function(req,res){
-   res.send('Article two reqested and will be served here'); 
+app.get('/song-two',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'song-two.html'));
 });
 
-app.get('/article-three',function(req,res){
-   res.send('Article three reqested and will be served here'); 
+app.get('/song-three',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'song-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
