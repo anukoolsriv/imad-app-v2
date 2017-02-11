81f8368f10a0ea8e7b4b8833d4a9b071241c188d
Anukool Srivastava <anukoolsriv@gmail.com>
2017-02-12 02:12:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,23 @@
 // Buttton code
 
 var button = document.getElementById('counter');
-var counter =0;
 button.onclick = function(){
-  //Make a request to counter end point
+  // create a request
+  var request = new XMLHttpRequest();
   
   //capture the response and store in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        // take some action
+        if(request.status ===200){
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+  };
   
-  //render variable in correct span
-  
-  counter = counter+1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
-  
-    
+  // make a request
+  request.open('GET','https://anukoolsriv.imad.hasura-app.io/counter',true);
+  request.send(null);
 };
