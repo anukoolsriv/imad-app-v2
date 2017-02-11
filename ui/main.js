a59684d38e06c4547ddf330ad9b9360976c4ea81
Anukool Srivastava <anukoolsriv@gmail.com>
2017-02-11 23:49:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,1 @@
 console.log('Loaded!');
-
-//change in main.js file
-
-//move image
-var img = document.getElementById('madi');
-marginl = 0;
-function right()
-{
-    marginl = marginl+2;
-    img.style.marginLeft = marginl+'px';
-}
-img.onclick = function(){
-    var interval = setInterval(right,50);
-};
