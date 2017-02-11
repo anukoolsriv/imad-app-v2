0209a6d061e6be59402ffe05ae1514db2f78dbc6
Anukool Srivastava <anukoolsriv@gmail.com>
2017-02-11 12:53:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,8 +101,10 @@ function createtemplate(data){
 return htmltemplate;
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+var counter=0;
+app.get('/counter', function (req, res) {
+  counter = counter+1;
+  res.send(counter.toString());
 });
 
 app.get('/:songname',function(req,res){
