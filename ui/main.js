105b6964464be6c7f01352a48b2a4f6d0923bb0f
Anukool Srivastava <anukoolsriv@gmail.com>
2017-02-12 00:36:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
-console.log('Loaded!');
+// Buttton code
+
+var button = document.getElementById('counter');
+var counter =0;
+button.onclick = function(){
+  //Make a request to counter end point
+  
+  //capture the response
+  
+  
+  //store in a variable
+  
+  //render variable in correct span
+  
+  counter = counter+1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
+  
+    
+};
