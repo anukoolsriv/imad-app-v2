946a72bbf71b753bc8e6be0fe0bc7cec70dceac7
Anukool Srivastava <anukoolsriv@gmail.com>
2017-02-12 22:17:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,21 +28,30 @@ var nameIP = document.getElementById('name');
 var name = nameIP.value;
 
 var submit = document.getElementById('submitbtn');
-
 submit.onclick = function(){
     //make a request
-    
-    
-    //capture list
-    var names=['name1','name2','name3','name4'];
-    
+    // create a request
+  var request = new XMLHttpRequest();
+  //capture the response and store in a variable
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        // take some action
+        if(request.status ===200){
+            //capture list
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list='';
     
     for(var i=0;i<names.length;i++)
     {
         list+='<li>'+names[i]+'</li>';
     }
-    
     var ul = document.getElementById('namelist');
     ul.innerHTML=list;
+        }
+    }
+  };
+  // make a request
+  request.open('GET','http://anukoolsriv.imad.hasura-app.io/submit-name'+name,true);
+  request.send(null);
 };
