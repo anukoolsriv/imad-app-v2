2d6d8f3b65a05e0fe4e42e99453197ade40eb381
Anukool Srivastava <anukoolsriv@gmail.com>
2017-02-14 23:43:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,7 @@ submit.onclick = function(){
     {
         list+='<li>'+names[i]+'</li>';
     }
-    var ul = document.getElementById('namelist');
+    var ul = document.getElementById('nameslist');
     ul.innerHTML=list;
         }
     }
