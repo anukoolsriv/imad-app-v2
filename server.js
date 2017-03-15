0b52a8a5def8d50332d00aba8e78d1fbb6984947
Anukool Srivastava <anukoolsriv@gmail.com>
2017-03-16 00:51:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-
+var crypto = require('crypto');
 
 var config  = {
     user: 'anukoolsriv',
@@ -57,6 +57,19 @@ return htmltemplate;
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'ui','index.html'));
+});
+
+function hash(input,salt){
+    // how to create hash
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input',function(req,res){
+   var hashString = hash(req.params.input,'this-is-some-random-string'); 
+   
+   res.send(hashString);
 });
 
 var pool = new Pool(config);
