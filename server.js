473f1f6e4dfbc04133d1ec8e0b7fb789ec4933f5
Anukool Srivastava <anukoolsriv@gmail.com>
2017-03-16 22:48:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
 
 var config  = {
     user: 'anukoolsriv',
@@ -14,6 +16,7 @@ var config  = {
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 function createtemplate(data){
     var title=data.title;
@@ -70,6 +73,25 @@ app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input, 'this-is-some-string'); // 'this-is-some-random-string' is a salt value
    res.send(hashedString);
 });
+
+app.post('/create-user',function(req,res){
+   //username,password
+   //JSON request
+   var username = req.body.usernae;
+   var password = req.body.password;
+   var salt = crypto.getRandomBytes(128).toString('hex');
+   var dbString = hash(password,salt);
+   pool.query('INSERT INTO "user"(username,password) VALUES($1,$2)',[username,dbString], function(req,res){
+       if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           res.send('User successfully created: '+username);
+       }
+   });
+});
+
+
 
 var pool = new Pool(config);
 
