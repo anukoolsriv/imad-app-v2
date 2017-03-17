ac87352a097f6857821066bf877462602d472bbd
Anukool Srivastava <anukoolsriv@gmail.com>
2017-03-17 21:43:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,28 +6,27 @@ submit.onclick = function(){
     // create a request
   var request = new XMLHttpRequest();
   //capture the response and store in a variable
-  request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-        // take some action
-        if(request.status ===200){
-            //capture list
-    console.log('user logged in');
-    alert('logged in successfully');
-        } else if (request.status === 403){
-            alert('password is incorrect');
-        } else if(request.status === 500){
-            alert('Boom! Something went terribly wrong!');
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            // take some action
+            if(request.status ===200){
+                //capture list
+                console.log('user logged in');
+                alert('logged in successfully');
+            } else if (request.status === 403){
+                alert('password is incorrect');
+            } else if(request.status === 500){
+                alert('Boom! Something went terribly wrong!');
+            }
         }
-        
-    }
-  };
+    };
   // make a request
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
     console.log(username);
     console.log(password);
-  request.open('POST','http://anukoolsriv.imad.hasura-app.io/login',true);
-  request.setRequestHeader('Content-Type','application/json');
-  request.send(JSON.stringify({username:username,password:password}));
+    request.open('POST', 'http://anukoolsriv.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
 };
