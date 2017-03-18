5899a81051f1e06e6d94b5f5cb2d4cc887d3d53b
Anukool Srivastava <anukoolsriv@gmail.com>
2017-03-18 19:09:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ submit.onclick = function(){
 
     console.log(username);
     console.log(password);
-    request.open('GET', 'http://anukoolsriv.imad.hasura-app.io/login', true);
+    request.open('POST', 'http://anukoolsriv.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type', 'application/json');
     request.send(JSON.stringify({username: username, password: password}));
 };
