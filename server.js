1cb37b296ee25c20055dccd3fca283e68e9ff11b
Anukool Srivastava <anukoolsriv@gmail.com>
2017-03-18 19:09:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/create-user',function(req,res){
    });
 });
 
-app.get('/login',function(req, res){
+app.post('/login',function(req, res){
    var username = req.body.username;
    var password = req.body.password;
    console.log("1234");
